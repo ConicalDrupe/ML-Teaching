c38adc7349a9a39b8230bd7e8c7b04346bc07f6a
ConicalDrupe <ChrisJacobBoon@gmail.com>
2024-04-21 14:43:36 -0400

    Clean Script

---
+++ time-series/src/clean_data.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+import os 
+
+df = pd.read_csv(os.path.join(os.getcwd(),'data','Hourly_clean.csv'))
+
+def check(df):
+    print(df.head())
+    print(df.info())
+    print(df.describe())
+
+def clean(df,save_path ='Hourly_clean2.csv'):
+    df['datetime'] = pd.to_datetime(df['Date'] + df['Time'], format='%m/%d/%Y%H:%M')
+    print(df.head())
+    df.to_csv(os.path.join(os.getcwd(),'data',save_path),index = False)
+
+### Structure ###
+#
+# check(df)
+# clean(df)
+# - save
